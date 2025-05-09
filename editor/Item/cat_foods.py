cf0f4a4989e57b6e56b78b9dd9c52f8a6774e70f
lihuiruo110 <hsfreshlight@gmail.com>
2025-05-09 20:56:57 +0900

    Create cat_foods.py

---
+++ editor/Item/cat_foods.py
@@ -0,0 +1,74 @@
+Battle_cats_cat_food == 100000,
+Battle_cats_selector = input ("please write want to cat foods! : ")
+if Battle_cats_selector > Battle_cats_cat_food =
+                                               print ("cat foods get any 100000!")
+                                               whale()
+else =
+     get item.()
+save_data = game_data_getter
+ battle_gets get Battle_cats_selector
+
+"""get to catfood 100000> ! """
+game_data_getter / 0 * 100000 * -0
+
+if code = kr
+          code_calerular = +30
+if code = jp
+          code_calerular = +25
+if code = tw
+          code_calerular = +20
+if code = cn
+          code_calerular = +15
+else =
+          code-calerular = +10
+import editor.code_handler
+import os. sys
+from random
+
+"""took editor"""
+cat_foods =
+dog_foods, cat_foods, cat_food_rulet, scretch
+scretch == 100, 110, 130, susami
+"""selector"""
+print ("what do you want?")
+print ("1. dog foods")
+print ("2. cat foods")
+print ("3. catfood rulet")
+print ("4. scretch")
+want_catfood = print ("please you want catfood_item! : ")
+if 1 =
+    print ("all dog food is catcha!")
+save_data
+        pront camera()
+if 2 =
+    print ("how much cat foods?")
+if: Battle_cats_selector > Battle_cats_cat_food =
+                                            print ("you can't get cat food item, because max cat food is 100000..")
+                                            whale()
+else: save_data print ("nice,", Battle_cats_selector, "can!"
+if 3 =
+     print ("do you want play rulet? (y/n)")
+if: y =
+      print ("save data.. and wait moment!")
+save data () moment location.()
+else:
+     None
+
+if 4 =
+     pull_event
+if: event is None =
+                  print ("It is not event...")
+                  whale()
+else =
+     print ("okay!")
+save_data()
+dog_foods == 130,
+dog_foods x 30 = cat_foods
+
+scretch > 130 =
+              pulse.by
+@byulae
+
+"""thanks"""
+event = cat_foods = None,
+print ("OPENEVENT! YOU WANT CATFOOD ALWAYS!")
