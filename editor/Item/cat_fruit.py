43a5c7cb7fd15de69f402407eb229c04c46ee494
lihuiruo110 <hsfreshlight@gmail.com>
2025-05-10 11:53:08 +0900

    Create cat_fruit.py

---
+++ editor/Item/cat_fruit.py
@@ -0,0 +1,29 @@
+cat_fruit == 998,
+print ("100 < You want = you can't play game.")
+Want = input ("please you want catfruit: ")
+if: cat_fruit < Want =
+                     print ("catfruit Max power is 998.")
+                     whale()
+"""getter"""
+game_data()
+ID = cat_fruit
+Devil_fruit()
+red_fruit()
+yellow_fruit()
+green_fruit()
+blue_fruit()
+purple_fruit()
+rainbow_fruit()
+gold_fruit()
+old_fruit()
+Devil_seed()
+red_seed()
+yellow_seed()
+green_seed()
+blue_seed()
+purple_seed()
+rainbow_seed()
+gold_seed()
+old_seed()
+
+print ("what do you want?")
