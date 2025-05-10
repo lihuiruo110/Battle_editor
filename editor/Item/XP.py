e704cd53fb03d43c21de792cf3fc230127189582
lihuiruo110 <hsfreshlight@gmail.com>
2025-05-10 11:23:11 +0900

    Create XP.py

---
+++ editor/Item/XP.py
@@ -0,0 +1,13 @@
+XP == 99999999,
+Want = input ("please you want XP? : ")
+if: XP < Want =
+              print ("XP max power is 99999999.")
+              whale()
+else:
+    print (f"okay,{Want}.")
+  save_data()
+XP = Minarge
+
+"""XP Getter"""
+battle_cats = Ponos/ <Dev Tools>
+XP = minarge/ponos
